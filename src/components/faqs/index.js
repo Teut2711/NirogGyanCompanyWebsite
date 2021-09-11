d97a46e9c8ccfa5f1780986930fe2a0eeb58ead7
vishal2612200 <vishalsh533@gmail.com>
2021-09-11 17:56:30 +0530

    Fix for FAQ page

--- src/components/faqs/index.js
+++ src/components/faqs/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import FrequentQuestions from './FrequentQuestions';
 import Form from './Form';
+import { Grid } from "@material-ui/core";
+
 export default function FAQS({ state }) {
 
     return <React.Fragment>
-        <HeroSection state={state.HeroSection} />
-        <FrequentQuestions state={state.FrequentQuestions} />
-        <Form state={state.Form} />
-    </React.Fragment>
+                <Grid container alignContent="center">
+                    <HeroSection state={state.HeroSection} />
+                    <FrequentQuestions state={state.FrequentQuestions} />
+                    <Form state={state.Form} />
+                </Grid>
+            </React.Fragment>
 }
